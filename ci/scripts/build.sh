a5bb64af9640b8084731286d6635462fd475c256
Matt Rout <matt_rout92@live.co.uk>
2017-08-25 09:54:12 +0100

    Update build script to copy everything in build dir

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,5 +5,5 @@ cwd=$(pwd)
 export GOPATH=$cwd/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-frontend-filter-dataset-controller
-  make build && cp build/dp-frontend-filter-dataset-controller $cwd/build
+  make build && cp build/* $cwd/build
 popd
