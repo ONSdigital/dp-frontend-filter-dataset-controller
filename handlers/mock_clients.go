5a5b9edd89afa8b7abf1c3e9be5c16cf255e2d1f
Matt Rout <matt_rout92@live.co.uk>
2017-09-19 11:45:52 +0100

    Update mock_clients to match client methods

--- handlers/mock_clients.go
+++ handlers/mock_clients.go
@@ -70,6 +70,19 @@ func (_mr *MockFilterClientMockRecorder) AddDimensionValues(arg0, arg1, arg2 int
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "AddDimensionValues", arg0, arg1, arg2)
 }
 
+// CreateJob mocks base method
+func (_m *MockFilterClient) CreateJob(_param0 string) (string, error) {
+	ret := _m.ctrl.Call(_m, "CreateJob", _param0)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CreateJob indicates an expected call of CreateJob
+func (_mr *MockFilterClientMockRecorder) CreateJob(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "CreateJob", arg0)
+}
+
 // GetDimension mocks base method
 func (_m *MockFilterClient) GetDimension(_param0 string, _param1 string) (filter.Dimension, error) {
 	ret := _m.ctrl.Call(_m, "GetDimension", _param0, _param1)
