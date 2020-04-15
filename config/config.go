43a0f69aa58d6826a7646b94c241ca04f95ea901
David Subiros <subiros.david@gmail.com>
2020-04-15 12:03:43 +0100

    Updated default healthcheck config and upgraded to latest dp-healthcheck verion

--- config/config.go
+++ config/config.go
@@ -43,8 +43,8 @@ func Get() (cfg *Config, err error) {
 		DownloadServiceURL:         "http://localhost:23600",
 		EnableDatasetPreview:       false,
 		GracefulShutdownTimeout:    5 * time.Second,
-		HealthCheckInterval:        10 * time.Second,
-		HealthCheckCriticalTimeout: time.Minute,
+		HealthCheckInterval:        30 * time.Second,
+		HealthCheckCriticalTimeout: 90 * time.Second,
 	}
 
 	return cfg, envconfig.Process("", cfg)
